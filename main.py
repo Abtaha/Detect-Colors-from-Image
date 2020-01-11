a902fa3aa674560dc0b9c0f0a120802886d48c99
Abtaha <abtaha2005@gmail.com>
2020-01-12 00:31:16 +0500

    Added

---
+++ main.py
@@ -0,0 +1,33 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+
+try:
+	input("Enter your image >> ")
+	image = cv2.imread("img.png")
+except:
+	print("Error, exitting")
+
+
+lower = np.array([0, 0, 0])
+
+upper = np.array([60, 60, 255])
+mask = cv2.inRange(image, lower, upper)
+red = cv2.bitwise_and(image, image, mask = mask)
+
+upper = np.array([60, 255, 60])
+mask = cv2.inRange(image, lower, upper)
+green = cv2.bitwise_and(image, image, mask = mask)
+
+upper = np.array([255, 60, 60])
+mask = cv2.inRange(image, lower, upper)
+blue = cv2.bitwise_and(image, image, mask = mask)
+
+cv2.imshow("red", red)
+cv2.imshow("green", green)
+cv2.imshow("blue", blue)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
